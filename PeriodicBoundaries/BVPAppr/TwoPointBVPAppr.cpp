780c8c2c99cc4a41565eed0c85bc65bb15f0bc5c
Brent Morrill Pearce <bpearce2@uwyo.edu>
2018-05-09 10:13:14 -0600

    finnally after much thought finished implementing the modified assemble reaction,

---
+++ PeriodicBoundaries/BVPAppr/TwoPointBVPAppr.cpp
@@ -0,0 +1,604 @@
+#include "tridiagonal_matrix.h"
+#include "twoPointBVP.h"
+#include "TwoPointBVPAppr.h"
+
+
+TwoPointBVPAppr::TwoPointBVPAppr(int N, const double * subintervallengths, 
+								const TwoPointBVP * prob)
+{
+	numsubintervals = N;
+	steplenghts = subintervallengths;
+	theproblem = prob;
+
+	// if either reaction or external force is present, then we need Delta x_i
+	if (theproblem->reaction_is_present() ||
+		theproblem->forcing_fucntion_is_present())
+	{
+		Deltax.resize(numsubintervals + 1);
+		Deltax[0] = 0.5 * steplenghts[0];
+		for (int i = 1; i < numsubintervals; i++)
+		{
+			Deltax[i] = 0.5*steplenghts[i - 1] + 0.5*steplenghts[i];
+		}
+
+		Deltax[numsubintervals] = 0.5 * steplenghts[numsubintervals - 1];
+	}
+	//This is x_i 
+	xcoord.resize(numsubintervals + 1);
+	double * domain = theproblem->get_domain();
+	xcoord[0] = domain[0];
+	for (int i = 1; i <= numsubintervals; i++)
+	{
+		xcoord[i] = xcoord[i - 1] + steplenghts[i - 1];
+	}
+
+	//This is x_{i -1/2} or x_{i + 1/2}
+	midcoord.resize(numsubintervals + 2);
+	midcoord[0] = domain[0];
+	for (int i = 1; i <= numsubintervals; i++)
+	{
+		midcoord[i] = 0.5 * (xcoord[i] + xcoord[i - 1]);
+	}
+	midcoord[N + 1] = xcoord[numsubintervals];
+}
+
+vector<double> TwoPointBVPAppr::get_xcoord()
+{
+	return xcoord;
+}
+
+int TwoPointBVPAppr::get_numsubintervals()
+{
+	return	numsubintervals;
+}
+
+void TwoPointBVPAppr::set_intial_guess_seed(double(*guessSeed)(vector<double> &))
+{
+	intialGuessSeed = guessSeed;
+	guess_seed_is_present = true;
+}
+
+double TwoPointBVPAppr::eval_intial_guess_seed(vector<double>& par) const
+{
+	return intialGuessSeed(par);
+}
+
+double TwoPointBVPAppr::AssembleDiffusion(tridiagonal_matrix *tmat)
+{
+	vector <double> kappa(numsubintervals);
+	vector <double> par(1);
+	for (int i = 0; i < numsubintervals; i++)
+	{
+		par[0] = midcoord[i + 1];
+		kappa[i] = theproblem->eval_diffusion(par);
+	}
+
+
+	for (int i = 0; i < numsubintervals; i++)
+	{
+		kappa[i] = kappa[i] / steplenghts[i];
+	}
+
+	//Take care of the three cases Left PBC, Right PBC and no PBC
+
+	if (theproblem->is_left_bdry_periodic())
+	{
+		// the first row of the matrix is ommited 
+		//the oneth row becomes the zeroth row 
+		tmat->set_diagonal_entry(0, kappa[1] + kappa[2]);
+		tmat->set_upper_diagonal_entry(0,  - kappa[0]);
+
+		//fill all the interior rows of the matrix
+		for (int i = 1; i < numsubintervals - 1; i++)//Indices May need 
+		{											 //work!					If not delete these comments!
+			// the interior points are the coeffs in the
+			// approximate soln given by eqaution star
+			tmat->set_lower_diagonal_entry(i - 1, -kappa[i]);
+			tmat->set_diagonal_entry(i, kappa[i] + kappa[i + 1]);
+			tmat->set_upper_diagonal_entry(i, -kappa[i+1]);
+		}
+		// it is either nuemen or robin (if nueman then gamma_n = 0)
+		double *RBV;
+		RBV = theproblem->get_right_bdry_values();
+		tmat->set_diagonal_entry(numsubintervals-1, 
+								 kappa[numsubintervals - 1] - RBV[0]);
+		tmat->set_lower_diagonal_entry(numsubintervals - 2, 
+									   -kappa[numsubintervals - 1]);
+
+		return -kappa[0];
+	}
+	else if (theproblem->is_right_bdry_periodic())
+	{
+		//filling up the first row
+		// the left bdry is Nueman or Robin
+		double *LBV;
+		LBV = theproblem->get_left_bdry_values();
+		tmat->set_diagonal_entry(0, kappa[0] - LBV[0]);
+		tmat->set_upper_diagonal_entry(0, -kappa[0]);
+		
+		//filling up the interior portion of the submatrix B
+		for (int i = 1; i < numsubintervals - 1; i++)
+		{
+			tmat->set_lower_diagonal_entry(i - 1, -kappa[i - 1]);
+			tmat->set_diagonal_entry(i, kappa[i] + kappa[i - 1]);
+			tmat->set_upper_diagonal_entry(i, -kappa[i]);
+		}
+
+		//filling the last row of th sub matrix
+		tmat->set_lower_diagonal_entry(numsubintervals - 2, 
+			-kappa[numsubintervals - 2]);
+		tmat->set_diagonal_entry(numsubintervals-1,
+			kappa[numsubintervals-1] + kappa[numsubintervals - 2]);
+		return kappa[numsubintervals - 1];
+	}
+	else
+	{
+		// taking care of the zeroth row(left boundary)
+		if (theproblem->left_bdry_is_Dirichlet())
+		{
+			// if the left boundary is Dirichlet then the 
+			//first row of the tridiagMat has coeffceint 
+			//1 for diag[1] and 0 for upperdiag[0]
+			tmat->set_diagonal_entry(0, 1.0);
+			tmat->set_upper_diagonal_entry(0, 0);
+		}
+		else
+		{
+			
+		}
+
+		//filling up the matrix tmat for internal rows
+		for (int i = 1; i <= numsubintervals - 1; i++)
+		{
+			// the interior points are the coeffs in the
+			// approximate soln given by eqaution star
+			tmat->set_lower_diagonal_entry(i - 1, -kappa[i - 1]);
+			tmat->set_diagonal_entry(i, kappa[i] + kappa[i - 1]);
+			tmat->set_upper_diagonal_entry(i, -kappa[i]);
+		}
+		// taking care of the last row(right boundary)
+		if (theproblem->right_bdry_is_Dirichlet())
+		{
+			// if the right bdry is Dirichlet then the 
+			// last row of the tridiagMat has coeff
+			// 1 for diag[N] and 0 for lowerdiag[N-1]
+			tmat->set_diagonal_entry(numsubintervals, 1.0);
+			tmat->set_lower_diagonal_entry(numsubintervals - 1, 0);
+		}
+		else
+		{
+			// it is either nuemen or robin (if nueman then gamma_n = 0)
+			double *RBV;
+			RBV = theproblem->get_right_bdry_values();
+			tmat->set_diagonal_entry(numsubintervals, kappa[numsubintervals - 1] - RBV[0]);
+			tmat->set_lower_diagonal_entry(numsubintervals - 1, -kappa[numsubintervals - 1]);
+		}
+		return 0.0;
+	}
+
+}
+
+void TwoPointBVPAppr::AssembleReaction(vector<double> &U,
+		vector<double> &RW, vector<double> &RPW )
+{
+
+	//U is our Solution, RW is the new reaction force and RPW is the partial derivative of RW
+
+	vector <double> par(2);
+	vector <double> val(2);
+
+	//taking care of the left boundary
+	if (theproblem->left_bdry_is_Dirichlet())
+	{
+		//if dirichlet Reaction[0] = 0
+		RW[0] = 0;
+		RPW[0] = 0;
+	}
+	else
+	{
+		//if not dirichlet Reaction[0] will be evaluated for par[0] and par2
+		par[0] = xcoord[0];
+		par[1] = U[0];
+		val =  theproblem->eval_reaction(par);
+		RW[0] = val[0] * Deltax[0];
+		RPW[0] = val[1] * Deltax[0];
+
+	}
+
+	// now the middle points
+	for (int i = 1; i < numsubintervals; i++)
+	{
+		par[0] = xcoord[i];
+		par[1] = U[i];
+		val = theproblem->eval_reaction(par);
+		RW[i] = val[0] * Deltax[i];
+		RPW[i] = val[1] * Deltax[i];
+	}
+
+	// now the right boundary
+	if (theproblem->right_bdry_is_Dirichlet())
+	{
+		//fill this
+		RW[numsubintervals] = 0;
+		RPW[numsubintervals] = 0;
+	}
+	else
+	{
+		//filling the necessary conditions for the right boundary
+		par[0]= xcoord[numsubintervals];
+		par[1] = U[numsubintervals];
+		val = theproblem->eval_reaction(par);
+		RW[numsubintervals] = val[0] * Deltax[numsubintervals];
+		RPW[numsubintervals] = val[1] * Deltax[numsubintervals];
+	}
+
+}
+
+vector<double> TwoPointBVPAppr::AssembleForce()
+{
+	vector <double> par(1);
+	 
+	//if there is a forcing function
+	// set all terms equal to forcing function
+	// or boundary conditions or both
+	if (theproblem->forcing_fucntion_is_present())
+	{
+		if (theproblem->is_left_bdry_periodic())
+		{
+			// This vector contains the force algebraic terms
+			vector<double> FF(numsubintervals);
+			
+			//zeroth row not neeeded;
+			
+			// fill entries 1-N-1
+			for (int i = 0; i < numsubintervals - 1; i++)
+			{
+				//for the interior points the FF is governed soley by itself
+				par[0] = xcoord[i+1];
+				FF[i] = theproblem->eval_forcing_function(par)*Deltax[i+1];
+			}
+
+			// fill row N
+			//it must be Nueman or Robin so the rhs is FF-g_L
+			double *RBC = theproblem->get_right_bdry_values();
+			par[0] = xcoord[numsubintervals];
+			FF[numsubintervals-1] = theproblem->eval_forcing_function(par)
+				*Deltax[numsubintervals] - RBC[1];
+
+			return FF;
+		}
+		
+		else if (theproblem->is_right_bdry_periodic())
+		{
+			vector<double> FF(numsubintervals);
+
+			//fill the zeroth row
+			//it must be Nueman or robin so the rhs[0] is FF-g_0
+			double *LBC = theproblem->get_left_bdry_values();
+			par[0] = xcoord[0];
+			FF[0] = theproblem->eval_forcing_function(par)
+				*Deltax[0] - LBC[1];
+			
+			//fill the 1 - N-1 entries
+			for (int i = 1; i < numsubintervals; i++)
+			{
+				//for the interior points the FF is governed soley by itself
+				par[0] = xcoord[i];
+				FF[i] = theproblem->eval_forcing_function(par)*Deltax[i];
+			}
+
+			//Nth entry not needed
+			
+			return FF;
+		}
+
+		else
+		{
+			vector<double> FF(numsubintervals + 1);
+
+			// Left boundary
+			if (theproblem->left_bdry_is_Dirichlet())
+			{
+				// when it is dirichlet the rhs[0] is simply g_0
+				double *LBC = theproblem->get_left_bdry_values();
+				FF[0] = LBC[1];
+			}
+			else
+			{
+				//if it is Nueman or robin then the rhs[0] is
+				// ff-g_0
+				double *LBC = theproblem->get_left_bdry_values();
+				par[0] = xcoord[0];
+				FF[0] = theproblem->eval_forcing_function(par)
+					*Deltax[0] - LBC[1];
+			}
+
+			//interior points
+			for (int i = 1; i < numsubintervals; i++)
+			{
+				//for the interior points the FF is governed soley by itself
+				par[0] = xcoord[i];
+				FF[i] = theproblem->eval_forcing_function(par)*Deltax[i];
+			}
+
+
+			// Right boundary
+			if (theproblem->right_bdry_is_Dirichlet())
+			{
+				//if it is Dirichlet then the rhs is g_L 
+				double *RBC = theproblem->get_right_bdry_values();
+				FF[numsubintervals] = RBC[1];
+			}
+			else
+			{
+				//if it is Nueman or Robin then the rhs is FF-g_L
+				double *RBC = theproblem->get_right_bdry_values();
+				par[0] = xcoord[numsubintervals];
+				FF[numsubintervals] = theproblem->eval_forcing_function(par)
+					*Deltax[numsubintervals] - RBC[1];
+			}
+			
+			return FF;
+		}
+	}
+
+	//if there isnt a ForcingFunct then set FF all
+	//equal to zero except when otherwise dictated by BCs.
+
+	else
+	{
+		if (theproblem->is_left_bdry_periodic())
+		{
+			vector<double> FF(numsubintervals);
+
+			//zeroth entry not needed 
+
+			//fill 1 - N-1 entries;
+			for (int i = 0; i < numsubintervals-1; i++)
+			{
+				//for the interior FF = 0
+				FF[i] = 0;
+			}
+
+			//fill the Nth entry
+			// the Nth Row must be Nuemmen or Robin so the rhs must be -g_L
+			double *RBC = theproblem->get_right_bdry_values();
+			FF[numsubintervals-1] = -RBC[1];
+			
+			return FF;
+		}
+		else if (theproblem->is_right_bdry_periodic())
+		{
+			vector<double> FF(numsubintervals);
+			
+			// fill the zeroth enrty
+			// it is Nueman or robin so the rhs[0] is -g_0
+			double *LBC = theproblem->get_left_bdry_values();
+			FF[0] = -LBC[1];
+
+			//fill the 1 - N-1 entries
+			for (int i = 1; i < numsubintervals; i++)
+			{
+				//for the interior FF = 0
+				FF[i] = 0;
+			}
+			
+			return FF;
+		}
+		else
+		{
+			vector<double> FF(numsubintervals + 1);
+
+			// Left boundary
+			if (theproblem->left_bdry_is_Dirichlet())
+			{
+				// when it is dirichlet the rhs[0] is simply g_0
+				double *LBC = theproblem->get_left_bdry_values();
+				FF[0] = LBC[1];
+			}
+			else
+			{
+				//if it is Nueman or robin then the rhs[0] is
+				// ff-g_0
+				double *LBC = theproblem->get_left_bdry_values();
+				FF[0] = -LBC[1];
+			}
+
+			//interior points
+			for (int i = 1; i < numsubintervals; i++)
+			{
+				//for the interior FF = 0
+				FF[i] = 0;
+			}
+
+			// Right boundary
+			if (theproblem->right_bdry_is_Dirichlet())
+			{
+				//if it is Dirichlet then the rhs[N] is g_L 
+				double *RBC = theproblem->get_right_bdry_values();
+				FF[numsubintervals] = RBC[1];
+			}
+			else
+			{
+				//if it is Nueman or Robin then the rhs is -g_L
+				double *RBC = theproblem->get_right_bdry_values();
+				FF[numsubintervals] = -RBC[1];
+			}
+
+			return FF;
+		}
+	}
+
+}
+
+double find_l2_norm(vector<double> const x)
+{
+	int num_entries = x.size();
+	double sum_o_squares = 0;
+	for (int i = 0; i < num_entries ; i++)
+	{
+		sum_o_squares = sum_o_squares + x[i] * x[i];
+	}
+	 return sqrt(sum_o_squares);
+}
+
+
+vector<double> TwoPointBVPAppr::Solve(int max_num_iter, double TOL)
+{
+	int iteration_counter = 0;
+	double norm;
+
+
+	if (theproblem->is_left_bdry_periodic()) //if the left bdry is periodic
+		// then use the modified newton's iteration.
+	{	
+		
+
+			
+	}
+	else if (theproblem->is_right_bdry_periodic()) // if the right bdry is 
+		//periodic then use the modified newton's iteration.
+	{
+
+	}
+	else
+	{
+	// Solution Method without PBCs 
+	
+
+	for (int iter = 1; iter <= max_num_iter; iter++)
+		{
+		
+		vector<double> G(numsubintervals + 1, 0.0);
+		vector<double> AU(numsubintervals + 1);
+
+		// Copy A to Gp
+		Gp = new tridiagonal_matrix(A);
+
+		// if there is a reaction calculate r(x,u) and pd(r(x,u),u)
+		if (theproblem->reaction_is_present())
+		{
+			AssembleReaction(U, R, Rp);
+			for (int i = 0; i < numsubintervals + 1; i++)
+				Gp->add_to_diagonal_entry(i, Rp[i]);
+		}
+
+		//Multiply the Matrix A and the vector U
+		AU = A->Mult(U); 
+
+		//for loop to  create each entry of the vector G
+		for (int i = 0; i <= numsubintervals; i++)
+		{
+			G[i] =  -1*(AU[i] + R[i] - F[i]);
+		}
+
+		//solve for h to update U
+		//first transform Gp
+		Gp->transform();
+
+		//solve the linear system for h
+		h = Gp->solve_linear_system(G);
+
+		//Update U
+		for (int i = 0; i <= numsubintervals; i++)
+		{
+			U[i] = U[i] + h[i];
+		}
+
+		//delete the Tridiagonal Matrix Gp associated with the iteration
+		delete Gp;
+
+		//find the norm of h to see if iterations continue
+		norm = find_l2_norm(h);
+
+		//determine if the condition ||U_n+1 - U_n|| < Tolerance has been met
+		if (norm < TOL)
+		{
+			// if met, break from loop and stop iterations
+			break;
+		}
+
+		// update iteration counter
+		iteration_counter = iter;
+	}
+	}
+	if (iteration_counter == max_num_iter)
+	{
+		std::ofstream ofs;
+		ofs.open("problem_info.txt", std::ofstream::out | std::ofstream::app);
+		ofs << " Convergence not reached within max number of iterations:  " << max_num_iter << endl;
+		ofs.close();
+	}
+	else
+	{
+		std::ofstream ofs;
+		ofs.open("problem_info.txt", std::ofstream::out | std::ofstream::app);
+		ofs << " Convergence was reached at iterations = " << iteration_counter << endl;
+		ofs.close();
+	}
+
+	delete A;
+
+	return U;
+}
+
+
+
+double TwoPointBVPAppr::find_max_error(int max_iters, double TOL)
+{
+	//generate an approximate solution
+	
+	vector<double> approximate_solution = Solve(max_iters, TOL);
+	int numberSubintervals = get_numsubintervals();
+
+	// Evaluate the true solution at all the xcoords
+	vector<double> true_solution(numberSubintervals);
+	vector<double> x(1);
+
+	for (int i = 0; i < numberSubintervals; i++)
+	{
+		x[0] = xcoord[i];
+		true_solution[i] = theproblem->eval_true_solution(x);
+	}
+
+	// Compare the true solution to the  approximate 
+	// solution and store/update the bigest error found
+	// durring the sweep.
+
+	// create error at x_i and intialize max error
+	double max_error =-1 ;
+	double ex_i;
+
+	//for loop to find and update max error
+	for (int i = 0; i < numberSubintervals; i++)
+	{
+		// calculate the current error
+		ex_i = fabs(true_solution[i] - approximate_solution[i]);
+		// compare the absolute value of the errors 
+		if (max_error < ex_i)
+		{
+			max_error = ex_i;
+		}
+
+	}
+
+	return max_error;
+}
+
+tridiagonal_matrix *TwoPointBVPAppr::calcDiffusion()
+{
+tridiagonal_matrix *tmat = new tridiagonal_matrix(numsubintervals+1);
+AssembleDiffusion(tmat);
+return tmat;
+}
+
+vector<double> TwoPointBVPAppr::calcLumpedMass()
+{
+
+	return vector<double>();
+}
+
+TwoPointBVPAppr::~TwoPointBVPAppr()
+{
+	;
+}
---
+++ PeriodicBoundaries/BVPAppr/twoPointBVP.cpp
@@ -0,0 +1,261 @@
+#include "twoPointBVP.h"
+
+
+// A C++ class implementation for two point BVPs
+
+TwoPointBVP::TwoPointBVP(double *dom, double(*dFunc) (vector<double> &))
+{
+	domain = dom;
+	diffusion = dFunc;
+	reactionIsPresent = false;
+	forcingFunctIsPresent = false;
+}
+
+void TwoPointBVP::set_left_bdry(bool _leftIsDirichlet, double *val)
+{
+	leftBdryIsDirichlet = _leftIsDirichlet;
+	leftBdryValues = val;
+}
+
+void TwoPointBVP::set_right_bdry(bool _rightIsDirichlet, double *val)
+{
+	rightBdryIsDirichlet = _rightIsDirichlet;
+	rightBdryValues = val;
+}
+
+void TwoPointBVP::set_left_periodic_bdry()
+{
+	leftBdryIsPeriodic = true;
+}
+
+void TwoPointBVP::set_right_periodic_bdry()
+{
+	rightBdryIsPeriodic = true;
+}
+
+void TwoPointBVP::set_reaction(double(*rFunctOne) (vector<double> &),
+							   double(*rFunctTwo) (vector<double> &))
+{
+	reaction = rFunctOne;
+	partialreactionpartialu = rFunctTwo;
+	reactionIsPresent = true;
+}
+
+void TwoPointBVP::set_forcing_function(double(*fFunct) (vector<double> &))
+{
+	forcingFunct = fFunct;
+	forcingFunctIsPresent = true;
+}
+
+void TwoPointBVP::set_true_solution(double(*TrueSol)(vector<double>&))
+{
+	trueSolu = TrueSol;
+	trueSolIsPresent = true;
+}
+
+double * TwoPointBVP::get_domain() const
+{
+	return domain;
+}
+
+bool TwoPointBVP::left_bdry_is_Dirichlet() const
+{
+	return leftBdryIsDirichlet;
+}
+
+bool TwoPointBVP::right_bdry_is_Dirichlet() const
+{
+	return rightBdryIsDirichlet;
+}
+
+bool TwoPointBVP::is_left_bdry_periodic() const
+{
+	return leftBdryIsPeriodic;
+}
+
+bool TwoPointBVP::is_right_bdry_periodic() const
+{
+	return rightBdryIsPeriodic;
+}
+
+double * TwoPointBVP::get_left_bdry_values() const
+{
+	return leftBdryValues;
+}
+
+double * TwoPointBVP::get_right_bdry_values() const
+{
+	return rightBdryValues;
+}
+
+bool TwoPointBVP::reaction_is_present() const
+{
+	return reactionIsPresent;
+}
+
+bool TwoPointBVP::forcing_fucntion_is_present() const
+{
+	return forcingFunctIsPresent;
+}
+
+bool TwoPointBVP::true_solution_is_present() const
+{
+	return trueSolIsPresent;
+}
+
+double TwoPointBVP::eval_diffusion(vector<double> &x) const
+{
+	return diffusion(x);
+}
+
+vector<double> TwoPointBVP::eval_reaction(vector<double> &par) const
+{
+	vector<double> val(2);
+	val[0] = reaction(par);
+	val[1] = partialreactionpartialu(par);
+	return val;	
+}
+
+double TwoPointBVP::eval_forcing_function(vector<double>& x) const
+{
+	return forcingFunct(x);
+}
+
+double TwoPointBVP::eval_true_solution(vector<double>& x) const
+{
+	return trueSolu(x);
+}
+
+void TwoPointBVP::display_info_TwoPointBVP() const
+{
+	ofstream fileout;
+	fileout.open("problem_info.txt");
+	fileout << "**********************************************************"
+		<<"************** \n";
+	fileout << " Some info regarding the two point BVP problem and" <<
+		"approximation: " << endl;
+
+
+	fileout << " Domain is:(" << domain[0] << "," << domain[1] << ")" << endl;
+
+	if (leftBdryIsPeriodic || rightBdryIsPeriodic) // if one of the BCs is a 
+	{											//PBC 
+
+		if (rightBdryIsPeriodic) // if right is a PBC find out and display
+								//whether left is nuemann or robin
+		{
+			if (leftBdryValues[0] == 0)
+			{
+				fileout << " Left Boundary is Neumann with g_0: " <<
+					leftBdryValues[1] << endl;
+			}
+			else
+			{
+				fileout << " Left Boundary is Robin with gamma_0: "
+					<< leftBdryValues[0] << " and g_0: " <<
+					leftBdryValues[1] << endl;
+			}
+			fileout << "Right Boundary is Periodic" << endl;
+		}
+		else //the left must be a PBC
+		{
+			fileout << "Left Boundary is Periodic" << endl;
+
+			//find out if Right BC is nuemann or robin and display which
+			if (rightBdryValues[0] == 0)
+			{
+				fileout << " Right Boundary is Neumann with g_L: "
+					<< rightBdryValues[1] << endl;
+			}
+			else
+			{
+				fileout << " Right Boundary is Robin with gamma_L: "
+					<< rightBdryValues[0] << " and g_L: "
+					<< rightBdryValues[1] << endl;
+			}
+
+
+		}
+
+	}
+	else //there aren't any PBCs
+	{
+		{} if (leftBdryIsDirichlet)
+		{
+			fileout << " Left boundary is Dirichlet with value: "
+				<< leftBdryValues[1] << endl;
+		}
+		else if (leftBdryValues[0] == 0)
+		{
+			fileout << " Left Boundary is Neumann with g_0: "
+				<< leftBdryValues[1] << endl;
+		}
+		else
+		{
+			fileout << " Left Boundary is Robin with gamma_0: "
+				<< leftBdryValues[0] << " and g_0: " <<
+				leftBdryValues[1] << endl;
+		}
+
+
+		if (rightBdryIsDirichlet)
+		{
+			fileout << " Right boundary is Dirichlet with value: "
+				<< rightBdryValues[1] << endl;
+		}
+		else if (rightBdryValues[0] == 0)
+		{
+			fileout << " Right Boundary is Neumann with g_L: "
+				<< rightBdryValues[1] << endl;
+		}
+		else
+		{
+			fileout << " Right Boundary is Robin with gamma_L: "
+				<< rightBdryValues[0] << " and g_L: "
+				<< rightBdryValues[1] << endl;
+		}
+	}
+	if (reactionIsPresent)
+	{
+		fileout << " Reaction is Present. " << endl;
+	}
+	else
+	{
+		fileout << " No reaction is present." << endl;
+	}
+
+	if (forcingFunctIsPresent)
+	{
+		fileout << " Forcing Function is Present. " << endl;
+	}
+	else
+	{
+		fileout << " Forcing function is not present. " << endl;
+	}
+	
+	fileout << "**********************************************************"
+		<< "**************" << endl;
+	fileout.close();
+	return;
+}
+
+
+
+vector<double> TwoPointBVP::true_solution(int numEvals, double domLeft, 
+														double domRight)
+{
+	vector<double> trueSoln(numEvals);
+	double steps = (domRight-domLeft) / (numEvals-1);
+	for (int i = 0; i < numEvals; i++)
+	{
+		vector<double> value(1);
+		value[0] = domLeft + i * steps;
+		trueSoln[i] = eval_true_solution(value);
+	}
+	return trueSoln;
+}
+
+TwoPointBVP::~TwoPointBVP()
+{
+	;
+}
