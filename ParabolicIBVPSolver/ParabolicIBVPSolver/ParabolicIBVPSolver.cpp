a64e8abf76eb48ec5d3bccc10026f2ea8985e9b8
Brent Morrill Pearce <bpearce2@uwyo.edu>
2018-05-29 13:32:46 -0600

    changed the names of the folder and the name of the int main file

---
+++ ParabolicIBVPSolver/ParabolicIBVPSolver/ParabolicIBVPSolver.cpp
@@ -0,0 +1,266 @@
+#include "tridiagonal_matrix.h"
+#include "twopointbvp.h"
+#include "twopointbvpappr.h"
+#include "ParabolicIBVPSolver.h"
+
+
+ParabolicIBVPSolver::ParabolicIBVPSolver(TwoPointBVPAppr * prob)
+{
+	apprbvp = prob;
+	diffusionmat = apprbvp->calcDiffusion(); // memory is created.
+}
+
+ParabolicIBVPSolver::~ParabolicIBVPSolver()
+{
+	delete diffusionmat;
+}
+
+//----------------------------------------------------------------------------
+
+ForwardEuler::ForwardEuler(TwoPointBVPAppr * prob) : ParabolicIBVPSolver(prob)
+{
+	cout << " A Forward Euler Scheme is utilized\n";
+}
+
+vector<double> ForwardEuler::one_step_march(vector<double> &timeinterval,
+	vector<double> &Ul)
+{
+	vector<double> Ur;
+	// implement here according the derivation we did in class (see class notes).
+
+	return Ur;
+}
+
+//----------------------------------------------------------------------------
+
+BackwardEuler::BackwardEuler(TwoPointBVPAppr * prob) : ParabolicIBVPSolver(prob)
+{
+	std::ofstream ofs;
+	ofs.open("problem_info.txt", std::ofstream::out | std::ofstream::app);
+	ofs << " A Backward Euler Scheme is ultilized\n";
+	ofs.close();
+}
+
+vector<double> BackwardEuler::one_step_march(vector<double> &timeinterval,
+	vector<double> &Ul)
+{
+	//create some needed vars
+	int numsubintervals = apprbvp->get_numsubintervals();
+	int iteration_counter = 0;
+	int max_iterations = 100;
+	double Toler = 1e-20;
+	double norm = 1e10;
+	tridiagonal_matrix *Gp, *A;
+	vector<double> R(numsubintervals + 1, 0.0);
+	vector<double>Rp(numsubintervals + 1, 0.0);
+	vector<double>F;
+	double deltaTime = timeinterval[1] - timeinterval[0];
+	A = new tridiagonal_matrix(numsubintervals + 1);
+	// Calculate the tridiagonal matrix coming from diffusion component.
+	A = apprbvp->calcDiffusion();
+
+	//create the left time solution and intial right time solution
+	vector<double> U_l = Ul;
+	vector<double> Ur = Ul;
+
+
+	// fill the force vector
+	F = apprbvp->calcForce(timeinterval[1]);
+
+	vector<double> lumpMassMatrix = apprbvp->calcLumpedMass();
+
+
+	vector<double> G(numsubintervals + 1);
+
+	//Newton Iteration solving Gp*h = G
+	while (iteration_counter <= max_iterations && norm>Toler)
+	{
+		Gp = new tridiagonal_matrix(A);
+
+		vector<double> AUr = A->Mult(Ur);
+
+		// if there is a reaction calculate r(x,u) and pd(r(x,u),u)
+		// (if there isn't then fill R and RP all zero)
+		apprbvp->calcReaction(Ur, R, Rp);
+
+		//create Gp (jacobian of vector mapping G)
+		for (int i = 0; i < numsubintervals + 1; i++)
+		{
+			Gp->add_to_diagonal_entry(i, Rp[i] +
+				(1 / deltaTime)*lumpMassMatrix[i]);
+		}
+
+		//create G(Ur)
+		for (int i = 0; i < numsubintervals + 1; i++)
+		{
+			G[i] = -1 * ((1 / deltaTime)*lumpMassMatrix[i] * (Ur[i] - Ul[i])) -
+				(AUr[i] + R[i] - F[i]);
+		}
+
+		for (int i = 0; i < numsubintervals + 1; i++)
+		{
+			Gp->add_to_diagonal_entry(i, (Rp[i] +
+				(1 / deltaTime)*lumpMassMatrix[i]));
+		}
+
+		//transform Gp so we can call solve_linear system
+		Gp->transform();
+
+		//solve for delta needed to update
+		vector<double> delta(numsubintervals + 1);
+		delta = Gp->solve_linear_system(G);
+
+		//Update Ur
+		for (int i = 0; i <= numsubintervals; i++)
+		{
+			Ur[i] = Ur[i] + delta[i];
+		}
+
+		//delete the Tridiagonal Matrix Gp associated with the iteration
+		delete Gp;
+
+		//find the norm of delta to see if iterations continue
+		norm = find_l2_norm(delta);
+
+		//update the iteration counter.
+		iteration_counter++;
+
+	}
+
+	if (iteration_counter == max_iterations)
+	{
+		std::ofstream ofs;
+		ofs.open("problem_info.txt", std::ofstream::out | std::ofstream::app);
+
+		ofs << " Convergence not reached within max number of iterations:  "
+			<< max_iterations << endl;
+
+		ofs.close();
+	}
+
+	return Ur;
+}
+
+//-------------------------------------------------------------------------
+
+ImplicitMidPoint::ImplicitMidPoint(TwoPointBVPAppr*prob) :ParabolicIBVPSolver(prob)
+{
+	cout << " An Implicit MidPoint Scheme is utilized\n";
+}
+
+vector<double> ImplicitMidPoint::one_step_march(vector<double> &timeinterval,
+	vector<double> &Ul)
+{
+	// create some needed vars
+	int numsubintervals = apprbvp->get_numsubintervals();
+	int iteration_counter = 0;
+	int max_iterations = 100;
+	double Toler = 1e-20;
+	double norm = 1e10;
+	tridiagonal_matrix *Gp, *A;
+	vector<double> R(numsubintervals + 1, 0.0);
+	vector<double>Rp(numsubintervals + 1, 0.0);
+	vector<double>F;
+	double deltaTime = timeinterval[1] - timeinterval[0];
+	A = new tridiagonal_matrix(numsubintervals + 1);
+	// Calculate the tridiagonal matrix coming from diffusion component.
+	A = apprbvp->calcDiffusion();
+
+	//create the left time solution intial right time solution and
+	//intial approximate midtime solution.
+	vector<double> U_l = Ul;
+	vector<double> Ur = Ul;
+
+
+
+	// fill the force vector
+	F = apprbvp->calcForce(timeinterval[1]);
+
+	vector<double> lumpMassMatrix = apprbvp->calcLumpedMass();
+
+
+	vector<double> G(numsubintervals + 1);
+
+	//Newton Iteration solving Gp*h = G
+	while (iteration_counter <= max_iterations && norm>Toler)
+	{
+		Gp = new tridiagonal_matrix(A);
+
+		vector<double> AUr = A->Mult(Ur);
+
+		// if there is a reaction calculate r(x,u) and pd(r(x,u),u)
+		// (if there isn't then fill R and RP all zero)
+		apprbvp->calcReaction(Ur, R, Rp);
+
+		for (int i = 0; i < numsubintervals + 1; i++)
+		{
+			Gp->add_to_diagonal_entry(i, Rp[i] +
+				(1 / deltaTime)*lumpMassMatrix[i]);
+		}
+		for (int i = 0; i < numsubintervals + 1; i++)
+		{
+			G[i] = -1 * ((1 / deltaTime)*lumpMassMatrix[i] * (Ur[i] - Ul[i])) -
+				(AUr[i] + R[i] - F[i]);
+		}
+
+		for (int i = 0; i < numsubintervals + 1; i++)
+		{
+			Gp->add_to_diagonal_entry(i, (Rp[i] +
+				(1 / deltaTime)*lumpMassMatrix[i]));
+		}
+
+		//transform Gp so we can call solve_linear system
+		Gp->transform();
+
+		//solve for delta needed to update
+		vector<double> delta(numsubintervals + 1);
+		delta = Gp->solve_linear_system(G);
+
+		//Update Ur
+		for (int i = 0; i <= numsubintervals; i++)
+		{
+			Ur[i] = Ur[i] + delta[i];
+		}
+
+		//delete the Tridiagonal Matrix Gp associated with the iteration
+		delete Gp;
+
+		//find the norm of delta to see if iterations continue
+		norm = find_l2_norm(delta);
+
+		//update the iteration counter.
+		iteration_counter++;
+
+	}
+
+	if (iteration_counter == max_iterations)
+	{
+		std::ofstream ofs;
+		ofs.open("problem_info.txt", std::ofstream::out | std::ofstream::app);
+
+		ofs << " Convergence not reached within max number of iterations:  "
+			<< max_iterations << endl;
+
+		ofs.close();
+	}
+
+
+
+	return Ur;
+}
+
+//----------------------------------------------------------------------------
+
+ExplicitMidPoint::ExplicitMidPoint(TwoPointBVPAppr * prob) : ParabolicIBVPSolver(prob)
+{
+	cout << " An Explicit MidPoint Scheme is utilized\n";
+}
+
+vector<double> ExplicitMidPoint::one_step_march(vector<double> &timeinterval,
+	vector<double> &Ul)
+{
+	vector<double> Ur;
+	vector<double> Um;
+
+	return Ur;
+}
---
+++ ParabolicIBVPSolver/ParabolicIBVPSolver/tridiagonal_matrix.h
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include"nonmember_functions.h"
+using namespace std;
+
+class tridiagonal_matrix
+{
+private:
+	int dimension;
+	vector <double> diag;
+	vector <double> upperdiag;
+	vector <double> lowerdiag;
+	vector <double> hatupperdiag; // modified upper diag entries
+	vector <double> r; // vector containing denominators
+	bool transformed;
+
+public:
+	tridiagonal_matrix(int m);
+
+	tridiagonal_matrix(const tridiagonal_matrix *mat);
+
+	int get_dimension() const;
+
+	void set_diagonal_entry(int i, double val);
+
+	void set_upper_diagonal_entry(int i, double val);
+
+	void set_lower_diagonal_entry(int i, double val);
+
+	double get_diagonal_entry(int i) const;
+
+	double get_upper_diagonal_entry(int i) const;
+
+	double get_lower_diagonal_entry(int i) const;
+
+	double get_r_entry(int i) const;
+
+	double get_hat_upper_diagonal_entry(int i) const;
+
+	bool is_transformed() const;
+
+	// diag[i] = diag[i] + val;
+	void add_to_diagonal_entry(int i, double val);
+
+	// upperdiag[i] = diag[i] + val;
+	void add_to_upper_diagonal_entry(int i, double val);
+
+	// lowerdiag[i] = diag[i] + val;
+	void add_to_lower_diagonal_entry(int i, double val);
+
+	void transform();
+
+	vector <double> solve_linear_system(const vector<double> & rhs) const;
+
+	// perform matrix vector multiplication
+	vector<double> Mult(const vector<double> & lhs) const;
+
+	~tridiagonal_matrix();
+};
